f9290ef0218b98152b38d5f9d630a7d59bb4b518
euandr <andrefariasalvino@gmail.com>
2024-11-04 19:12:31 -0300

    organizando

--- proc. img_pillow/todas_imgs.py
+++
@@ -1,9 +0,0 @@
-import PIL.Image as p
-import glob
-
-# caminho = r"C:\PythonSM\proc. img_pillow\img\*.*"
-caminho = "img/*.*"
-
-for img in glob.glob(caminho):
-   carregando = p.open(img)
-   carregando.show()
